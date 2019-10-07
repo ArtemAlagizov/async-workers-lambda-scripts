5ebe5db495ab6380583850be4c4e9696db128133
ArtemAlagizov <alagizov@gmail.com>
2019-10-07 21:26:10 +0200

    updated e2e_tests.sh

--- ec2_scripts/e2e_tests.sh
+++ ec2_scripts/e2e_tests.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -xe
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
-#ping -q -c5 3.13.90.180 > /dev/null
-#
-#if [ $? -eq 0 ]
-#then
-#	echo "ok"
-#fi
+sudo yum install -y git
+# git clone e2e tests
+# execute e2e tests
+# send logs and report through sns service (/var/log/user-data.log)
+# if tests are succesful => trigger lambda to tag docker images and upload dem images to docker hub
 
 aws ec2 terminate-instances --instance-ids `curl http://169.254.169.254/latest/meta-data/instance-id` --region 'us-east-2'
