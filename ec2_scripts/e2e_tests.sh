f064a4239c40bbf30c7c2fa3ae6c252efafdfae9
ArtemAlagizov <alagizov@gmail.com>
2019-10-08 10:52:16 +0200

    updated e2e_tests.sh, added messages for sns

--- ec2_scripts/e2e_tests.sh
+++ ec2_scripts/e2e_tests.sh
@@ -8,10 +8,10 @@ sudo yum install -y git
 # execute e2e tests
 # send logs and report through sns service (/var/log/user-data.log)
 
-aws sns publish --message "ttt" --phone-number +31681440850 --region 'us-east-2'
+aws sns publish --message "ttt" --phone-number +31XXX --region 'us-east-1'
 } || {
 # if tests are succesful => trigger lambda to tag docker images and upload dem images to docker hub
 ## aws lambda invoke --function-name ReleaseVersionUpdateFunction --invocation-type Event --payload "[JSON string here]"
 
-# aws ec2 terminate-instances --instance-ids `curl http://169.254.169.254/latest/meta-data/instance-id` --region 'us-east-2'
+aws ec2 terminate-instances --instance-ids `curl http://169.254.169.254/latest/meta-data/instance-id` --region 'us-east-2'
 }
