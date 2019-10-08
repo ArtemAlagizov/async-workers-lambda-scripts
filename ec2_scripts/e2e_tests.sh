8abf371eeb643bb5bf354217694812ad8583a4e4
ArtemAlagizov <alagizov@gmail.com>
2019-10-08 08:12:02 +0200

    updated e2e_tests.sh

--- ec2_scripts/e2e_tests.sh
+++ ec2_scripts/e2e_tests.sh
@@ -5,6 +5,8 @@ sudo yum install -y git
 # git clone e2e tests
 # execute e2e tests
 # send logs and report through sns service (/var/log/user-data.log)
+
+aws sns publish --message "ttt" --phone-number +31681440850
 # if tests are succesful => trigger lambda to tag docker images and upload dem images to docker hub
 ## aws lambda invoke --function-name ReleaseVersionUpdateFunction --invocation-type Event --payload "[JSON string here]"
 
