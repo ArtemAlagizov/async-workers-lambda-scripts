47af8eccd643cdce28b84b9b49e3e75491d48b29
ArtemAlagizov <alagizov@gmail.com>
2019-10-08 10:55:46 +0200

    updated e2e_tests.sh

--- ec2_scripts/e2e_tests.sh
+++ ec2_scripts/e2e_tests.sh
@@ -5,13 +5,18 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 {
 sudo yum install -y git
 # git clone e2e tests
+
+git clone https://github.com/ArtemAlagizov/async-workers-lambda-scripts.git
 # execute e2e tests
 # send logs and report through sns service (/var/log/user-data.log)
+cd messages
 
-aws sns publish --message "ttt" --phone-number +31XXX --region 'us-east-1'
+aws sns publish --message "ttt" --phone-number +31681440850 --region 'us-east-1'
+aws ses send-email --from sendah@aws.com --destination file://destination.json --message file://success_message.json
 } || {
 # if tests are succesful => trigger lambda to tag docker images and upload dem images to docker hub
 ## aws lambda invoke --function-name ReleaseVersionUpdateFunction --invocation-type Event --payload "[JSON string here]"
 
-aws ec2 terminate-instances --instance-ids `curl http://169.254.169.254/latest/meta-data/instance-id` --region 'us-east-2'
+echo "test"
+# aws ec2 terminate-instances --instance-ids `curl http://169.254.169.254/latest/meta-data/instance-id` --region 'us-east-2'
 }
