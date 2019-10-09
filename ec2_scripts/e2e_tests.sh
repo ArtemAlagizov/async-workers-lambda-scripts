ba61fcebe876e8eb556a5fe03e7cb5877ecc80a6
ArtemAlagizov <alagizov@gmail.com>
2019-10-09 08:27:36 +0200

    updated e2e_tests.sh

--- ec2_scripts/e2e_tests.sh
+++ ec2_scripts/e2e_tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -xe
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
-
 {
 sudo yum install -y git
 # git clone e2e tests
@@ -14,9 +13,12 @@ cd messages
 
 #aws sns publish --message "e2e tests were successful" --phone-number +316XXXXXXXX --region 'us-east-1'
 aws ses send-email --from alagizov@gmail.com --destination file://destination.json --message file://success_message.json --region 'us-east-1'
-} || {
 # if tests are succesful => trigger lambda to tag docker images and upload dem images to docker hub
 ## aws lambda invoke --function-name ReleaseVersionUpdateFunction --invocation-type Event --payload "[JSON string here]"
 
-aws ec2 terminate-instances --instance-ids `curl http://169.254.169.254/latest/meta-data/instance-id` --region 'us-east-2'
+} || {
+# log failure
+
 }
+
+aws ec2 terminate-instances --instance-ids `curl http://169.254.169.254/latest/meta-data/instance-id` --region 'us-east-2'
